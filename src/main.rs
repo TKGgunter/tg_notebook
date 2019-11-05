6fd83f4dbba35f226ac4d54fe19a74d57eb3ffbe
TKGgunter <thothgunter@live.com>
2019-11-04 19:49:41 -0600

    Rendering a window
    Render a window and initial setup of render code.

---
+++ src/main.rs
@@ -0,0 +1,231 @@
+#[macro_use]
+extern crate glium;
+extern crate nalgebra;
+
+use nalgebra::core::Matrix4;
+
+
+//TODO
+//what is the time cost of loading various resolutions of textures all the time
+
+use glium::{glutin, Surface};
+
+pub struct WindowInfo{
+    pub focused: bool,
+    pub width: i32,
+    pub height: i32,
+}
+
+struct Renderer<'a>{
+    display: &'a glium::Display,
+    target:  &'a mut glium::Frame,
+    indices: &'a glium::index::NoIndices,
+    program: &'a glium::Program,
+}
+
+#[derive(Copy, Clone)] 
+struct Vertex {
+    pub position: [f32; 2],
+    pub tex_coords: [f32; 2],
+}
+implement_vertex!(Vertex, position, tex_coords);
+
+fn generate_plane( x1: f32, x2: f32, y1: f32, y2: f32, display: &glium::Display)->glium::VertexBuffer<Vertex>{
+    let vertex1 = Vertex{ position: [x1, y1], tex_coords: [0.0, 0.0]};
+    let vertex2 = Vertex{ position: [x1, y2], tex_coords: [0.0, 1.0]};
+    let vertex3 = Vertex{ position: [x2, y2], tex_coords: [1.0, 1.0]};
+    let vertex4 = Vertex{ position: [x2, y1], tex_coords: [1.0, 0.0]};
+    let vertex5 = Vertex{ position: [x2, y2], tex_coords: [1.0, 1.0]};
+    let vertex6 = Vertex{ position: [x1, y1], tex_coords: [0.0, 0.0]};
+    let shape = vec![vertex1, vertex2, vertex3, vertex4, vertex5, vertex6];
+
+    return glium::VertexBuffer::new(display, &shape).expect("could not generate vertex buffer");
+}
+
+
+fn main(){
+
+    let mut event_loop = glutin::EventsLoop::new();
+    let monitor = event_loop.get_primary_monitor();
+    let glutin::dpi::PhysicalSize{width: monitor_width, height: monitor_height} = monitor.get_dimensions();
+
+    let _window = glutin::WindowBuilder::new()
+                    .with_dimensions(glutin::dpi::LogicalSize{width:monitor_width / 2.0, height:monitor_height - 20.0})
+                    .with_title("tg_notebook");
+    let context = glutin::ContextBuilder::new();
+    let display = glium::Display::new(_window, context, &event_loop).unwrap();
+
+
+
+    //NOTE use this to change the cursor we will need when we move and resize
+    // https://docs.rs/glutin/0.21.0/glutin/struct.Window.html#method.set_cursor
+    display.gl_window().window().set_position(glutin::dpi::LogicalPosition{x:monitor_width/2.0, y:0.0}); 
+ 
+
+    //NOTE
+    //original source code stolen from
+    //https://github.com/glium/glium/blob/master/examples/tutorial-10.rs
+    let vertex_shader_src = r#"
+        #version 140
+        
+        in vec2 position;
+        in vec2 tex_coords;
+        out vec2 v_tex_coords;
+
+        uniform mat4 perspective;
+        uniform mat4 matrix;
+       
+        void main(){
+            v_tex_coords = tex_coords;
+            gl_Position = perspective * matrix * vec4(position, 0.0, 1.0);
+        } 
+    "#;
+
+    let fragment_shader_src = r#"
+        #version 140
+    
+        in vec2 v_tex_coords;
+        out vec4 color;
+      
+        uniform sampler2D tex;
+        void main(){
+            color = texture(tex, v_tex_coords);
+        }
+    "#;
+
+
+    let mut windowinfo = WindowInfo{focused: true, width: 0, height: 0};
+    let mut exit = false;
+
+    let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).expect("Could not compile shaders");
+    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+    'gameloop: loop{
+
+        event_loop.poll_events(|event| {
+            match event {
+                glutin::Event::DeviceEvent{ event, ..} => { 
+                    match event { 
+                        glutin::DeviceEvent::Key(kb) =>{ 
+                            //NOTE 
+                            //scancode 1 is the code for Esc
+                            if kb.scancode == 1 && windowinfo.focused {exit = true;};
+                        },
+                        _=> () 
+                    }
+                },
+                glutin::Event::WindowEvent { event, .. } => match event {
+                    glutin::WindowEvent::CloseRequested => exit = true,
+                    glutin::WindowEvent::Focused(_bool) => windowinfo.focused = _bool,
+                    _=>{}
+                    //glutin::WindowEvent::CursorMoved{position, ..} => {
+                    //    let _pos : (i32, i32) = position.into();
+                    //    mouseinfo.x = _pos.0;
+                    //    mouseinfo.y = _pos.1;
+                    //},
+                },
+                _=>{}
+            }
+        });
+
+        let mut target = display.draw();
+        target.clear_color(0.0, 0.0, 0.0, 1.0);
+        //struct Renderer<'a>{
+        //    display: &'a glium::Display,
+        //    target:  &mut 'a glium::Frame,
+        //    indices: glium::index::NoIndices,
+        //    program: glium::Program,
+        //}
+
+        {
+
+            let renderer = Renderer{  display: &display, 
+                                      target:  &mut target, 
+                                      indices: &indices,
+                                      program: &program,
+                                   }; 
+            TEMP_FN(renderer); //&mut target, &display, &indices);
+        }
+
+        target.finish().unwrap();
+        if exit == true{break 'gameloop} 
+    }
+
+
+}
+
+
+fn TEMP_FN( mut renderer: Renderer ){
+
+    let bmp = Bmp{ w: 10, h: 10, buffer: vec![150u8; 4*10*10]};
+    gl_drawbmp(&mut renderer, &bmp, 0.0, 0.0, None);
+}
+
+//TODO TEMP
+struct Bmp{
+    pub w: u32,
+    pub h: u32,
+    pub buffer: Vec<u8>,
+}
+
+//TODO
+//Think about this
+fn gl_drawbmp( renderer: &mut Renderer, bmp: &Bmp, x: f32, y: f32, perspective: Option<[[f32;4];4]> )->glium::texture::Texture2d{
+    let Renderer{display, target, indices, program} = renderer;
+
+    let w = bmp.w;
+;   let h = bmp.h;
+
+    let ratio = w as f32 / h as f32;
+
+    let image = glium::texture::RawImage2d::from_raw_rgba(bmp.buffer.clone(), (w, h));//I would rather not clone but what can you do.
+
+    //TODO Store this value on to this and drop only when we update the bmps
+    let texture = glium::texture::Texture2d::new(*display, image).expect("we could not make the texture");
+
+    let mut _perspective = perspective.unwrap_or(
+                          [  [1.0, 0.0, 0.0, 0.0,],
+                             [0.0, 1.0, 0.0, 0.0,],
+                             [0.0, 0.0, 1.0, 0.0,],
+                             [0.0, 0.0, 0.0, 1.0f32]]
+                          );
+    //TODO
+    //need an x and y scaling term
+    let     transform = Matrix4::new(1.0, 0.0, 0.0, 0.0,
+                                     0.0, 1.0, 0.0, 0.0,
+                                     0.0, 0.0, 1.0, 0.0,
+                                       x,   y, 1.0, 1.0);
+     
+
+    fn populate_array_using_matrix(in_matrix: &Matrix4<f32>, out_array: &mut [[f32;4];4]){
+        //TODO
+        //move out at some point
+        for (i, it) in in_matrix.iter().enumerate(){
+            let _i = i / 4;
+            let _j = i % 4;
+            out_array[_j][_i] = *it;
+        }
+    }
+
+    let mut _matrix = [[0.0f32; 4]; 4];
+    populate_array_using_matrix( &transform, &mut _matrix);
+    {
+        let uniforms = uniform!{ 
+            perspective: _perspective,	
+            matrix: _matrix,
+            tex: &texture,
+        };
+        
+        //TODO Is this ok? it's prob slow right?
+        let vertexbuffer = generate_plane(0.0, ratio, 0.0, 1.0, display);
+
+        target.draw(&vertexbuffer, *indices, program, &uniforms, &Default::default()).unwrap();
+    }
+
+
+    return texture;
+}
+
+
+
+
+
